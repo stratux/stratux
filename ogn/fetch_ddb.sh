edb5ab6a9f8dacf6de4f72756aafceb0c18979a2
Jim Jacobsen <jamez1970@gmail.com>
2025-02-06 11:23:47 -0600

    Place a copy of ddb.json into the repository since OGN download was failing. If the download fails, use the backup copy

--- ogn/fetch_ddb.sh
+++ ogn/fetch_ddb.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+echo "Fetching ddb.json from ddb.glidernet.org"
 wget -O ddb.json http://ddb.glidernet.org/download/?j=1
+if [ "$?" -ne 0 ]; then
+	echo "Error downloading the file. Using the existing copy"
+	cp -f ddb.json.copy ddb.json
+fi
+exit 0
