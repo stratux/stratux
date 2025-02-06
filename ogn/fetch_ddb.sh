80cb1f0ce2f9ff11b7227115ba625e935f6bb850
Jim Jacobsen <jamez1970@gmail.com>
2025-02-06 11:25:31 -0600

    Script cleanup

--- ogn/fetch_ddb.sh
+++ ogn/fetch_ddb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Fetching ddb.json from ddb.glidernet.org"
-wget -O ddb.json http://ddb.glidernet.org/download/?j=1
+wget -qO ddb.json http://ddb.glidernet.org/download/?j=1
 if [ "$?" -ne 0 ]; then
-	echo "Error downloading the file. Using the existing copy"
+	echo "Error downloading the file. Using the existing copy stored in ddb.json.copy"
 	cp -f ddb.json.copy ddb.json
 fi
 exit 0
